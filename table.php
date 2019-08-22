867efa27f45779c82dd82c666fda657e41a293a0
muthuakilesh-it19 <muthuakilesh.it19@bitsathy.ac.in>
2019-08-22 08:41:36 +0530

    ¨fourthcommit¨

---
+++ table.php
@@ -0,0 +1,10 @@
+<?php
+$no = 5;
+echo"<table>";
+for($i=1; $i<=30; ++$i){
+echo "<tr>"."<td>".$i."</td><td>*</td><td>$no</td><td>=</td><td>". ($i * $no) ."</td></tr><br />";
+echo"</table>";
+}
+
+
+?>
